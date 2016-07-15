57ddcb7b85ecc7c13d7ae1dabfb608b6dd36fcd2
Minkah Harun Oliveira <minkah10@gmail.com>
2016-07-15 18:29:36 -0300

    mudança na function.php

--- src/model/forms/functions.php
+++ src/model/forms/functions.php
@@ -7,7 +7,7 @@ require_once '../entity/Mercadoria.php';
 $operacao = $_POST['operacao'];
 $qtd_operacao = $_POST['qtd_operacao'];
 $cod_mercadoria = $_POST['cod_mercadoria'];
-$tipo_negocio = 'Venda';//$_POST['tipo_negocio'];
+$tipo_negocio = $_POST['tipo_negocio'];
 
 $mercadoria = new mercadoria_dao();
 //$conn =
@@ -16,7 +16,7 @@ try {
     if (isset($operacao) and isset($qtd_operacao)) {
         switch ($operacao) {
             case 'Compra':
-                if (isset($tipo_negocio) and $tipo_negocio ='Compra'){
+                if (isset($tipo_negocio) and $tipo_negocio ==='Compra'){
                 $qtd_comprada = $qtd_operacao;
                 $conn = new mercadoria_dao();
                 $conn->executaCompra($cod_mercadoria, $qtd_comprada);
@@ -24,7 +24,7 @@ try {
                 "<h1>Produto não disponivel para compra</h1>";
                 break;
             case 'Venda':
-                if (isset($tipo_negocio) and $tipo_negocio ='Venda'){
+                if (isset($tipo_negocio) and $tipo_negocio ==='Venda'){
                     $qtd_vendida = $qtd_operacao;
                     $conn = new mercadoria_dao();
                     $conn->executaVenda($cod_mercadoria, $qtd_vendida);
